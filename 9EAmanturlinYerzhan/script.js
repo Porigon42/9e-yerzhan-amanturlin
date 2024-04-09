1604129611bdae7145d770ab2eaec72c7346d3e6
Porigon42 <166480065+Porigon42@users.noreply.github.com>
2024-04-09 12:16:52 +0600

    Add files via upload

---
+++ 9EAmanturlinYerzhan/script.js
@@ -0,0 +1,6 @@
+styledimg {
+    background-image: url(https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3JtNTg2YmF0Y2gyLWVtb2ppLTAwNi5wbmc.png);
+    background-repeat: no-repeat;
+    width: 100px;
+    height: 200px;
+    }
